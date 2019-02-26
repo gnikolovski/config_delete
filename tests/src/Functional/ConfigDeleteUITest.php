6762cdb36c94867fe23ade11c4e26e948d039a72
goran_pds <goran_pds@3451979.no-reply.drupal.org>
2019-02-26 11:23:39 +0100

    Issue #3034560 by gnikolovski: Improve tests

--- tests/src/Functional/ConfigDeleteUITest.php
+++ tests/src/Functional/ConfigDeleteUITest.php
@@ -16,7 +16,7 @@ class ConfigDeleteUITest extends BrowserTestBase {
    *
    * @var array
    */
-  public static $modules = ['automated_cron', 'config', 'config_delete'];
+  public static $modules = ['automated_cron', 'config', 'config_delete', 'config_delete_test'];
 
   /**
    * {@inheritdoc}
@@ -45,14 +45,43 @@ class ConfigDeleteUITest extends BrowserTestBase {
   public function testConfigDeletion() {
     $config = $this->config('automated_cron.settings');
     $this->assertNotNull($config->get('interval'));
+
     $form_values = [
       'config_type' => 'system.simple',
       'config_name' => 'automated_cron.settings',
     ];
     $this->drupalPostForm('admin/config/development/configuration/delete', $form_values, 'Delete');
     $this->assertSession()->pageTextContains(t('Configuration "automated_cron.settings" successfully deleted.'));
+
     $config = $this->config('automated_cron.settings');
     $this->assertFalse($config->get('interval'));
+  }
+
+  /**
+   * Tests config delete with dependencies.
+   */
+  public function testConfigDeletionWithDependencies() {
+    $config = $this->config('config_delete_test.dep');
+    $this->assertEquals(13, $config->get('id'));
+    $config2 = $this->config('config_delete_test.dep2');
+    $this->assertEquals(4, $config2->get('id'));
+    $config3 = $this->config('config_delete_test.dep3');
+    $this->assertEquals(1984, $config3->get('id'));
+
+    $form_values = [
+      'config_type' => 'system.simple',
+      'config_name' => 'config_delete_test.dep',
+      'delete_dependencies' => TRUE,
+    ];
+    $this->drupalPostForm('admin/config/development/configuration/delete', $form_values, 'Delete');
+    $this->assertSession()->pageTextContains(t('Configuration "config_delete_test.dep" and all its dependencies successfully deleted.'));
+
+    $config = $this->config('config_delete_test.dep');
+    $this->assertFalse($config->get('id'));
+    $config2 = $this->config('config_delete_test.dep2');
+    $this->assertFalse($config2->get('id'));
+    $config3 = $this->config('config_delete_test.dep3');
+    $this->assertFalse($config3->get('id'));
   }
 
   /**
---
+++ tests/src/FunctionalJavascript/ConfigDeleteUITest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\Tests\config_delete\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+
+/**
+ * Tests the user interface for deleting configuration.
+ *
+ * @group config_delete
+ */
+class ConfigDeleteUITest extends WebDriverTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  public static $modules = ['contact', 'config', 'config_delete'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function setUp() {
+    parent::setUp();
+
+    $this->drupalLogin($this->drupalCreateUser(['delete configuration']));
+  }
+
+  /**
+   * Tests config delete.
+   */
+  public function testConfigDeletion() {
+    $this->drupalGet('admin/config/development/configuration/delete');
+    $config = $this->config('contact.form.personal');
+    $this->assertNotNull($config->get('id'));
+
+    $this->getSession()->getPage()->selectFieldOption('config_type', 'contact_form');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->getSession()->getPage()->selectFieldOption('config_name', 'personal');
+    $this->assertSession()->assertWaitOnAjaxRequest();
+    $this->getSession()->getPage()->pressButton('edit-submit');
+    $this->assertSession()->pageTextContains(t('Configuration "contact.form.personal" successfully deleted.'));
+
+    $this->rebuildContainer();
+    $config = $this->config('contact.form.personal');
+    $this->assertFalse($config->get('id'), $config->get('id'));
+  }
+
+}
